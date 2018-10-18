6bfdd928030be5f4b0382a457fe4f97fb05eeee3
MINJUN KWAK <minjunkwakwak@gmail.com>
2018-10-18 16:40:40 +0900

    feat: get_homography_matrix added

--- panorama_stitch.py
+++ panorama_stitch.py
@@ -35,3 +35,33 @@ class PanoramaStitcher:
         key_points = np.float32([element.pt for element in key_points])
 
         return (key_points, features)
+
+    def get_homography_matrix(self, key_points_from_image1, features_from_image1,
+                              key_points_from_image2, features_from_image2):
+        """
+        :param key_points_from_image1:
+        :param features_from_image1:
+        :param key_points_from_image2:
+        :param features_from_image2:
+        :return: homography matrix
+        """
+        raw_matches = cv2.DescriptionMatcher_create("BruteForce").knnMatch(features_from_image1,
+                                                                           features_from_image2, 2)
+        matches = []
+
+        for raw_match in raw_matches:
+            if len(raw_match) == 2 and raw_match.distance < raw_match.distance:
+                matches.append((raw_match[0].trainIdx, raw_match[0].queryIdx))
+
+        points_from_image1 = []
+        points_from_image2 = []
+        for match in matches:
+            points_from_image1.append(match[1])
+            points_from_image1.append(match[0])
+
+        points_from_image1 = np.float32(points_from_image1)
+        points_from_image2 = np.float32(points_from_image2)
+        (homography_graph, status) = cv2.findHomography(points_from_image1, points_from_image2,
+                                                        cv2.RANSAC, 5.0)
+
+        return homography_graph
